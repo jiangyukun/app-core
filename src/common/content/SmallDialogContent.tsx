19235ad895d355517afd0a65e8672aef5f7701d7
jiangyukun <187_6810_5877@sina.com>
2017-11-21 17:40:30 +0800

    update

--- src/common/content/SmallDialogContent.tsx
+++ src/common/content/SmallDialogContent.tsx
@@ -2,13 +2,14 @@
  * Created by jiangyukun on 2017/5/26.
  */
 import React from 'react'
+import classnames from 'classnames'
 import Transition from '../../modal/Transition'
 import './small-dialog-content.scss'
 
 export default (props) => {
   return (
     <Transition show={props.show}>
-      <div className="my-modal-content small-dialog-content">
+      <div className={classnames('my-modal-content small-dialog-content', props.className || '')}>
         {props.children}
       </div>
     </Transition>
